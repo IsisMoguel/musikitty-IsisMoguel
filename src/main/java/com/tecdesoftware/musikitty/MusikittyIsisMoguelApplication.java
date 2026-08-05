28bf20485706fc23f9cc3d563ce52c582ee8e043
IsisMoguel <isis.moguel@tecdesoftware.edu.mx>
2026-08-04 21:06:56 -0600

    chore: initialize Spring Boot project with Maven dependencies

---
+++ src/main/java/com/tecdesoftware/musikitty/MusikittyIsisMoguelApplication.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.musikitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikittyIsisMoguelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusikittyIsisMoguelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecdesoftware/musikitty/MusikittyIsisMoguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.musikitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusikittyIsisMoguelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
