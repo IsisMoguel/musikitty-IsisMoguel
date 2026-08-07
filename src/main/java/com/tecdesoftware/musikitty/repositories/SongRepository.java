1a71f704e487c802d6a2b65dee18a8978e47b076
IsisMoguel <isis.moguel@tecdesoftware.edu.mx>
2026-08-06 23:28:06 -0600

    feat: implement Spring Data JPA repositories for Playlist and Song

--- src/main/java/com/tecdesoftware/musikitty/repositories/SongRepository.java
+++ src/main/java/com/tecdesoftware/musikitty/repositories/SongRepository.java
@@ -4,9 +4,6 @@ import com.tecdesoftware.musikitty.entities.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
-    List<Song> findByPlaylistId(Long playlistId);
 }
